1acf53bdb7f507e550068085c2f158d0e464397c
3Tron <3tron@amapz.com>
2017-07-23 14:32:47 +0200

    v1.0.15 optional debug mode

--- README.md
+++ README.md
@@ -5,13 +5,15 @@ Unblur progressively an object depending on load state
 ```
 import {decreasingBlur} from 'decreasingblur';
 
+const debugMode = false;
+
 load(url) {
     let xobj = new XMLHttpRequest();
     xobj.overrideMimeType('application/json');
     xobj.open('GET', url, true);
     xobj.onreadystatechange = function () {
         
-        decreasingBlur(xobj.readyState, this.refs.toggle);
+        decreasingBlur(xobj.readyState, this.refs.toggle, debugMode);
         
         if (xobj.readyState < 4) {
             //do nothing or something
@@ -22,3 +24,5 @@ load(url) {
     }.bind(this);
 }
 ```
+1.0.15
+added third optional bool parameter for debugging.
--- src/rui.js
+++ src/rui.js
@@ -1,26 +1,28 @@
-exports.decreasingBlur = function (state, obj) {
-  state = Number(state);
-  if (state === 0) {
-    obj.style.WebkitFilter = 'blur(6px)';
-    obj.style.Filter = 'blur(5px)';
+exports.decreasingBlur = function (state, obj, debug) {
+  if (debug !== undefined && debug !== false) {
+    console.log('decreasingBlur', state, obj);
   }
-  if (state === 1) {
-    obj.style.WebkitFilter = 'blur(4px)';
+  if (state == 0) {
+    obj.style.WebkitFilter = 'blur(10px)';
+    obj.style.Filter = 'blur(10px)';
+  }
+  if (state == 1) {
+    obj.style.WebkitFilter = 'blur(8px)';
+    obj.style.Filter = 'blur(8px)';
+  }
+  if (state == 1) {
+    obj.style.WebkitFilter = 'blur(6px)';
+    obj.style.Filter = 'blur(6px)';
+  }
+  if (state == 2) {
+    obj.style.WebkitFilter = 'blur4px)';
     obj.style.Filter = 'blur(4px)';
   }
-  if (state === 1) {
-    obj.style.WebkitFilter = 'blur(3px)';
-    obj.style.Filter = 'blur(3px)';
-  }
-  if (state === 2) {
+  if (state == 3) {
     obj.style.WebkitFilter = 'blur(2px)';
     obj.style.Filter = 'blur(2px)';
   }
-  if (state === 3) {
-    obj.style.WebkitFilter = 'blur(1px)';
-    obj.style.Filter = 'blur(1px)';
-  }
-  if (state === 4) {
+  if (state == 4) {
     obj.style.WebkitFilter = 'blur(0px)';
     obj.style.Filter = 'blur(0px)';
   }
