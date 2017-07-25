6318680eb9dcb581caecfe7af137c6f8da4dd711
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2017-07-25 21:53:25 +0200

    nicer

--- src/rui.js
+++ src/rui.js
@@ -1,11 +1,12 @@
 exports.decreasingBlur = function(state, obj, debug) {
   var httpStatusBlur = [
-    6, //status 0
-    4, //status 1
-    2, //status 2
+    8, //status 0
+    6, //status 1
+    4, //status 2
+    2, //status 3
     0, //status 4 DONE
   ];
-  var blur = 'blur(' + httpStates[state] + 'px)';
+  var blur = 'blur(' + httpStatusBlur[state] + 'px)';
   obj.style.WebkitFilter = blur;
   obj.style.Filter = blur;
   if (debug !== undefined && debug !== false) {
