4f5b4fd1f5d6e4f500d1a1f0c16cbef9ecd59910
Jean-Philippe Ruijs <jean-philippe@ruijs.fr>
2017-07-25 21:42:57 +0200

    v1.0.21

--- src/rui.js
+++ src/rui.js
@@ -1,26 +1,13 @@
-exports.decreasingBlur = function (state, obj, debug) {
-  var blur = null;
-  switch (Number(state)) {
-    case 0:
-      blur = 'blur(10px)';
-      break;
-    case 1:
-      blur = 'blur(8px)';
-      break;
-    case 2:
-      blur = 'blur(6px)';
-      break;
-    case 3:
-      blur = 'blur(4px)';
-      break;
-    case 4:
-      blur = 'blur(0px)';
-      break;
-  }
-  if (blur !== null) {
-    obj.style.WebkitFilter = blur;
-    obj.style.Filter = blur;
-  }
+exports.decreasingBlur = function(state, obj, debug) {
+  var httpStatusBlur = [
+    6, //status 0
+    4, //status 1
+    2, //status 2
+    0, //status 4 DONE
+  ];
+  var blur = 'blur(' + httpStates[state] + 'px)';
+  obj.style.WebkitFilter = blur;
+  obj.style.Filter = blur;
   if (debug !== undefined && debug !== false) {
     console.log('decreasingBlur', state, blur, obj);
   }
